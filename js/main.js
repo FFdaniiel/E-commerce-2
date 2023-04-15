6ce6d8cde1f38d15de76b2ff68842d3a812b48a9
Daniel Sanduay <daniel_alexis_12@hotmail.com>
2023-04-15 13:24:18 -0300

    slider y otras cosas

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Slider
+const radio = document.querySelector('#radio');
+let contador = 1;
+setInterval(function() {
+    document.querySelector('#radio' + contador).checked = true;
+    contador++
+    if(contador > 3){
+        contador = 1;
+    }
+}, 5000);
