8ceed592b77b6ba600a79af1a68de6c5a7cd404c
Daniel Sanduay <daniel_alexis_12@hotmail.com>
2023-04-15 13:38:01 -0300

    Fix slider

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ let contador = 1;
 setInterval(function() {
     document.querySelector('#radio' + contador).checked = true;
     contador++
-    if(contador > 3){
+    if(contador >= 3){
         contador = 1;
     }
 }, 5000);
